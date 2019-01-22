7636c272d417ef89298af07864e317ee9de7cd7f
Yulong Wang <yulongwang12@gmail.com>
2019-01-22 10:53:31 +0800

    change patterns weights url
    Since the original links fail, I upload the previously downloaded weights to Amazon S3.

--- weights/download_patterns.sh
+++ weights/download_patterns.sh
@@ -1,2 +1,2 @@
-wget https://www.dropbox.com/s/cvjj8x19hzya9oe/imagenet_224_vgg_16.npz?dl=1
-wget https://www.dropbox.com/s/v7e0px44jqwef5k/imagenet_224_vgg_16.patterns.A_only.npz?dl=1
+wget https://s3.cn-north-1.amazonaws.com.cn/modelzoo/imagenet_224_vgg_16.npz
+wget https://s3.cn-north-1.amazonaws.com.cn/modelzoo/imagenet_224_vgg_16.patterns.A_only.npz
